70a43fef77ad3e007c356d6fd18d50cb08495966
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-11-25 10:02:39 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        if(a == b && b == c)
-            System.out.print(10000 + a * 1000);
-        else if(a == b || a == c)
-            System.out.print(1000 + a * 100);
-        else if(b == c)
-            System.out.print(1000 + b * 100);
-        else
-            System.out.print((Math.max(Math.max(a, b), c) * 100));
-    }
-    
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		sc.close();
+		
+		if(a == b && b == c && a == c){
+			System.out.println(10000 + a * 1000);
+		} else if(a == b || a == c || b == c) {
+			int same = (a == b || a == c) ? a : b; 
+			System.out.println(1000 + same * 100);
+		} else {
+			int max = Math.max(a, Math.max(b, c));
+			System.out.println(max * 100);
+		}
+	}
+
 }
