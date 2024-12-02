fb07b2f9658225f4dd112a5c4bf497b1698a78f4
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-12-02 11:24:54 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,21 +1,44 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		while(true) {
+//			int A = sc.nextInt();
+//			int B = sc.nextInt();
+//			
+//			if(A == 0 && B == 0) {
+//				sc.close();
+//				break;
+//			}
+//			
+//			System.out.println(A + B);
+//		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer sc;
+		StringBuilder sb = new StringBuilder();
 		
-		while(true) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
+		while(true){
+			sc = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(sc.nextToken());
+			int B = Integer.parseInt(sc.nextToken());
 			
 			if(A == 0 && B == 0) {
-				sc.close();
 				break;
 			}
 			
-			System.out.println(A + B);
+			sb.append(A + B).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 }
