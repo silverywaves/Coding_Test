b2b4924f8615608b146fafc0b653471515296f25
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-12-31 08:05:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] arg) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		br.close();
+		
+		int ascii = (int)str.charAt(0);
+		
+		System.out.println(ascii);
+	}
+
+}
